ac6f921e122d7d8f19fc88ff019cd5a1470ca81d
Tawfik Y <tawfekyassertawfek@gmail.com>
2022-02-09 14:44:06 +0200

    Update Adminstrating Hadoop.sh

--- Show More/Adminstrating Hadoop.sh
+++ Show More/Adminstrating Hadoop.sh
@@ -12,3 +12,20 @@ hdfs dfs -mkdir /Hadoop_Sentiment
 hdfs dfs -copyFromLocal /usr/Hadoop_Sentiment/bucket_hs.txt /Hadoop_Sentiment
 
 Next: Hive, IntelliJ
+
+
+
+-----
+Main Hadoop Congf.
+
+Group: hadoopenv => sudo addgroup hadoopenv
+User: hduser => sudo adduser --ingroup hadoopenv hduser
+Password: 9520hadoop
+
+sudo visudo - add the user - CTRL X - Y - ENTER
+su - hduser => to use the new user
+
+
+to check dfs namenode: http://10.0.2.15:9870/
+to check yarn: http://10.0.2.15:8042
+to check dfs datanodes: http://10.0.2.15:9864/
